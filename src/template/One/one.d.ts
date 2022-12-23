0e087e9f731fe1680b9cf9ad1ecca73a5447270b
Nikita Radzkou <nikita.radzkou@gmail.com>
2022-12-23 19:06:42 +0300

    Init template

---
+++ src/template/One/one.d.ts
@@ -0,0 +1,26 @@
+export interface TProps {
+    theme: {
+        color: string;
+        fontFamily: string;
+    };
+    userData: {
+        name: string;
+        position: string;
+        infoTitle: string;
+        address: string;
+        email: string;
+        mobile: string;
+        userData: string;
+        profileTitle: string;
+        profile: string;
+        workExperienceTitle: string;
+        educationTitle: string;
+        skillsTitle: string;
+    };
+    workExperience: [];
+    education: [];
+    skills: [];
+    itemStatus: {
+        [key: string]: boolean;
+    };
+}
